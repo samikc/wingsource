8580df25af303d0a9c8144a12e27b6cc0ccdf52e
samikc <samikc@c8787748-f84e-11de-b622-b70f56c379d3>
2010-02-14 12:12:21 +0000

    PluginCrawler checkin

--- PluginProject/src/main/java/org/wingsource/plugin/engine/PluginCrawler.java
+++ PluginProject/src/main/java/org/wingsource/plugin/engine/PluginCrawler.java
@@ -18,6 +18,9 @@
 package org.wingsource.plugin.engine;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
@@ -29,6 +32,8 @@ import javax.xml.bind.Unmarshaller;
 import org.wingsource.plugin.SymbolResolverService;
 import org.wingsource.plugin.lang.xml.crawler.Crawler;
 import org.wingsource.plugin.lang.xml.crawler.Required;
+import org.wingsource.plugin.lang.xml.wingsite.Symbol;
+import org.wingsource.plugin.lang.xml.wingsite.Wingsite;
 
 /**
  * Works with the {@link PluginExplorer} and its crawler.xml file. It crawls the web 
@@ -58,12 +63,50 @@ public class PluginCrawler {
 				// We do not have a symbol resolver as of now so lets get it
 				try {
 					URL url = new URL(r.getWingsite());
-					
+					Wingsite wingsite = this.getSite(url);
+					URL jarUrl = null;
+					for (Symbol symbol :wingsite.getSymbol()) {
+						if (symbol.getName().equals(r.getSymbol())) {
+							jarUrl = new URL(symbol.getJarUrl());
+							InputStream is = jarUrl.openStream();
+							String jarName = jarUrl.toString().substring(jarUrl.toString().lastIndexOf("/"));
+							File fJar = new File(new StringBuilder().append(System.getProperty("user.home"))
+																	.append(File.separator)
+																	.append(PluginExplorer.PLUGIN_HOME_DIRECTORY_NAME)
+																	.append(File.separator)
+																	.append(jarName).toString());
+							FileOutputStream fos = new FileOutputStream(fJar);
+							int x = 0;
+							while(( x = is.read())!= -1) {
+								fos.write(x);
+							}
+							break;
+						}
+					}
 				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (JAXBException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
 		}
+	}
+	
+	private Wingsite getSite(URL url) throws JAXBException{
+		JAXBContext context = JAXBContext.newInstance("org.wingsource.plugin.lang.wingsite");
+		Unmarshaller unmarshaller = context.createUnmarshaller();
+		Wingsite wingsite = null;
+		try {
+			wingsite = (Wingsite) unmarshaller.unmarshal(url.openStream());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return wingsite;
 	}
 }
