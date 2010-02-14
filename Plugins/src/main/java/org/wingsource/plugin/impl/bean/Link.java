16304252841bd88f617774082ddb1eea17d39361
pillvin <pillvin@c8787748-f84e-11de-b622-b70f56c379d3>
2010-02-14 11:52:57 +0000

    Initial dump

---
+++ Plugins/src/main/java/org/wingsource/plugin/impl/bean/Link.java
@@ -0,0 +1,51 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.wingsource.plugin.impl.bean;
+
+/**
+ * @author samikc
+ *
+ */
+public class Link {
+
+	private String id;
+	private String url;
+	private String type;
+	private String pointerAttribute;
+	private String tag;
+	private static final String NEWLINE ="\n";
+
+	public Link(String id, String url, String type, String pointerAttribute,
+			String tag) {
+		super();
+		this.id = id;
+		this.url = url;
+		this.type = type;
+		this.pointerAttribute = pointerAttribute;
+		this.tag = tag;
+	}
+	
+	public String toString() {
+		StringBuilder sbuild = new StringBuilder();
+		sbuild.append("<link type='").append(type).append("' id='").append(id).append("'");
+		sbuild.append(" tag='").append(tag).append("' pattr='").append(pointerAttribute).append("' ");
+		sbuild.append(" url='").append(url).append("'");
+		sbuild.append(" />").append(NEWLINE);
+		return sbuild.toString();
+	}
+}
