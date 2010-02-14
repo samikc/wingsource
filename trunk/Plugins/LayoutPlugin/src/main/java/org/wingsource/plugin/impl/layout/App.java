c8e5a4123a4f735f0bb21c65cdf42243c3cfe756
pillvin <pillvin@c8787748-f84e-11de-b622-b70f56c379d3>
2010-02-14 12:40:36 +0000

    initial dump

---
+++ trunk/Plugins/LayoutPlugin/src/main/java/org/wingsource/plugin/impl/layout/App.java
@@ -0,0 +1,13 @@
+package org.wingsource.plugin.impl.layout;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
