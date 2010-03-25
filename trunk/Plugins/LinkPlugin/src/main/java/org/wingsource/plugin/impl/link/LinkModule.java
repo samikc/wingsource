4b19d0392d01a724c1f44cf3a25a559172495cd9
samikc <samikc@c8787748-f84e-11de-b622-b70f56c379d3>
2010-03-25 16:59:20 +0000

    properties for module support

--- trunk/Plugins/LinkPlugin/src/main/java/org/wingsource/plugin/impl/link/LinkModule.java
+++ trunk/Plugins/LinkPlugin/src/main/java/org/wingsource/plugin/impl/link/LinkModule.java
@@ -1,6 +1,8 @@
 package org.wingsource.plugin.impl.link;
 
 
+import org.wingsource.PropertiesClassLoader;
+
 import com.google.inject.Binder;
 import com.google.inject.Module;
 import com.google.inject.Scope;
@@ -8,8 +10,15 @@ import com.google.inject.Scope;
 public class LinkModule implements Module {
 
 	public void configure(Binder binder) {
-		// TODO Auto-generated method stub
-		binder.bind(LinkService.class).to(DummyLinkService.class);
+		Class c = DummyLinkService.class;
+		try {
+			PropertiesClassLoader pcl = new PropertiesClassLoader("module.properties");
+			c = pcl.getClazz("linkplugin");
+		}catch (Exception e) {
+			// We do not do anything just load the default class
+		}
+		//binder.bind(GadgetService.class).to(c);
+		binder.bind(LinkService.class).to(c);
 	}
 
 }
